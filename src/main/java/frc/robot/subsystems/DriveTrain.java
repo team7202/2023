51b922c3bee6f67b4186a2bdf85020d1ae8c6ed6
Cyber Orioles <100726118+team7202@users.noreply.github.com>
2023-03-07 17:08:57 -0500

    Made DriveTrain more commandy than subsystemy

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,184 +4,42 @@
 
 package frc.robot.subsystems;
 
-import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
-
-  public final CANSparkMax rearLeft;
-  public final CANSparkMax frontLeft;
-  public final CANSparkMax frontRight;
-  public final CANSparkMax rearRight;
-
-  public MotorControllerGroup leftGroup;
-  public MotorControllerGroup rightGroup;
-
-  private DifferentialDrive drive;
-
-  private Joystick leftJoystick;
-  private Joystick rightJoystick;
-
-  public final AHRS gyro;
-  private final PIDController pid;
-
-  private float pitch;
-  private float roll;
-  private float yaw;
-  private double angle;
-
   /** Creates a new DriveTrain. */
-  public DriveTrain(Joystick leftJoystick, Joystick rightJoystick) {
-    this.frontLeft = new CANSparkMax(Constants.DriveTrain.FRONT_LEFT_ID, Constants.DriveTrain.FRONT_LEFT_TYPE);
-    this.rearLeft = new CANSparkMax(Constants.DriveTrain.REAR_LEFT_ID, Constants.DriveTrain.REAR_LEFT_TYPE);
-    this.frontRight = new CANSparkMax(Constants.DriveTrain.FRONT_RIGHT_ID, Constants.DriveTrain.FRONT_RIGHT_TYPE);
-    this.rearRight = new CANSparkMax(Constants.DriveTrain.REAR_RIGHT_ID, Constants.DriveTrain.REAR_RIGHT_TYPE);
+  
+  private final CANSparkMax rearLeftMax;
+  private final CANSparkMax frontLeftMax;
+  private final CANSparkMax frontRightMax; 
+  private final CANSparkMax rearRightMax;
 
-    this.leftGroup = new MotorControllerGroup(rearLeft, frontLeft);
-    this.rightGroup = new MotorControllerGroup(rearRight, frontRight);
-    this.leftGroup.setInverted(Constants.DriveTrain.LEFT_GROUP_INVERTED);
-    this.rightGroup.setInverted(Constants.DriveTrain.RIGHT_GROUP_INVERTED);
+  private final MotorControllerGroup leftGroup;
+  private final MotorControllerGroup rightGroup;
 
+  private final DifferentialDrive drive;
+  
+  public DriveTrain() {
+    this.rearLeftMax = new CANSparkMax(Constants.CAN.REAR_LEFT_DRIVE, MotorType.kBrushed);
+    this.frontLeftMax = new CANSparkMax(Constants.CAN.FRONT_LEFT_DRIVE, MotorType.kBrushed);
+    this.frontRightMax = new CANSparkMax(Constants.CAN.FRONT_RIGHT_DRIVE, MotorType.kBrushed);
+    this.rearRightMax = new CANSparkMax(Constants.CAN.REAR_RIGHT_DRIVE, MotorType.kBrushed);
+    this.leftGroup = new MotorControllerGroup(rearLeftMax, frontLeftMax);
+    this.rightGroup = new MotorControllerGroup(rearRightMax, frontRightMax);
+    this.rightGroup.setInverted(true);
     this.drive = new DifferentialDrive(leftGroup, rightGroup);
-
-    this.leftJoystick = leftJoystick;
-    this.rightJoystick = rightJoystick;
-
-    this.gyro = new AHRS(Constants.DriveTrain.GYRO_PORT);
-    gyro.reset();
-
-    SmartDashboard.putNumber("rotate", 0);
-
-    this.pid = new PIDController(Constants.DriveTrain.kP, Constants.DriveTrain.kI, Constants.DriveTrain.kD);
-    this.pid.enableContinuousInput(-180.0f, 180.0f);
-
-    this.pitch = gyro.getPitch();
-    this.roll = gyro.getRoll();
-    this.yaw = gyro.getYaw();
-    this.angle = gyro.getAngle();
-    this.frontLeft.getEncoder().setPosition(0);
-    this.frontRight.getEncoder().setPosition(0);
   }
 
   @Override
-  public void periodic() {
-    this.pitch = gyro.getPitch();
-    this.roll = gyro.getRoll();
-    this.yaw = gyro.getYaw();
-
-    SmartDashboard.putNumber("frontLeftPos", frontLeft.getEncoder().getPosition());
-    SmartDashboard.putNumber("frontRightPos", frontRight.getEncoder().getPosition());
-    SmartDashboard.putNumber("roll", gyro.getPitch());
-    SmartDashboard.putNumber("yaw", gyro.getYaw());
-    SmartDashboard.putNumber("angle", gyro.getAngle());
-
-    // if(gyro.getPitch()> 10) {
-    // tipOverride = true;
-    // tankDrive(0.75, 0.75);
-    // } else if(gyro.getPitch() < -10) {
-    // tipOverride = true;
-    // tankDrive(-0.75, -0.75);
-    // } else {
-    // tipOverride = false;
-    // }
-
-    // if (gyro.getPitch() < -9) {
-    // drive.tankDrive(0.3, 0.3);
-    // } else if (gyro.getPitch() > 9) {
-    // drive.tankDrive(-0.3, -0.3);
-    // }
-
-    SmartDashboard.putNumber("pitch", gyro.getPitch());
-
-    if (leftJoystick.getRawButton(2)) {
-      rotateToAngle(-180);
-      return;
-    }
-
-    if (leftJoystick.getTrigger()) {
-      this.drive.tankDrive(-0.35, -0.35);
-      return;
-    } else if (Math.abs(leftJoystick.getY()) >= 0.05 || Math.abs(rightJoystick.getY()) >= 0.05) {
-      // double difference = Math.abs(leftJoystick.getY() - rightJoystick.getY());
-      // this.drive.tankDrive(Math.copySign(Math.pow(leftJoystick.getY(), 2),
-      // leftJoystick.getY()) * 0.7,
-      // Math.copySign(Math.pow(rightJoystick.getY(), 2), rightJoystick.getY()) *
-      // 0.7);
-      // if (leftJoystick.getY() < rightJoystick.getY())
-      // drive.tankDrive(leftJoystick.getY() + calculateDifference(),
-      // rightJoystick.getY());
-      // else
-      // drive.tankDrive(leftJoystick.getY() + calculateDifference(),
-      // rightJoystick.getY());
-      // return;
-      // } else {
-      drive.tankDrive(leftJoystick.getY() * 0.85, rightJoystick.getY() * 0.85);
-      // }
-    }
-
-    if (rightJoystick.getRawButtonPressed(16)) {
-      gyro.reset();
-    }
-
-    // if (leftJoystick.getRawButtonPressed(7)) {
-    // this.left.getEncoder().setPosition(0);
-    // this.right.getEncoder().setPosition(0);
-    // }
-
-    // if(rightJoystick.getRawButtonPressed()) {
-    // this.armMode = "off";
-    // }
-  }
-
-  public double calculateDifference() {
-    return 0.4 * Math.abs(leftJoystick.getY() - rightJoystick.getY());
-  }
-
-  public float getPitch() {
-    return pitch;
-  }
-
-  public float getRoll() {
-    return roll;
-  }
-
-  public float getYaw() {
-    return yaw;
-  }
-
-  public double getAngle() {
-    return angle;
-  }
+  public void periodic() {}
 
   public void tankDrive(double leftSpeed, double rightSpeed) {
     this.drive.tankDrive(leftSpeed, rightSpeed);
-  }
-
-  public void rotateToAngle(double angle) {
-    var val = MathUtil.clamp(this.pid.calculate(Math.round(this.gyro.getYaw()), Math.round(angle)), -.5, .5);
-    // var val = MathUtil.clamp(this.pid.calculate(this.gyro.getYaw(), angle), -0.5,
-    // 0.5);
-    this.drive.tankDrive(-val, val);
-  }
-
-  public double ticksToFeet(double feet) {
-    return feet / Constants.DriveTrain.FEET_DIVISOR;
-  }
-
-  public double percentToFormula(double percent) {
-    if (percent >= 0) {
-      return (0.6 * (Math.cbrt(percent - 0.5))) + 0.4762;
-    } else {
-      return -((0.6 * (Math.cbrt(-percent - 0.5))) + 0.4762);
-    }
   }
 }
