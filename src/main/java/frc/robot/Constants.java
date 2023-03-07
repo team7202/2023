f04aaa11da96bcc2bf3ee3acbcb0339bf044db30
Cyber Orioles <100726118+team7202@users.noreply.github.com>
2023-03-07 17:04:01 -0500

    Constants redo coming up!

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,51 +17,17 @@ import edu.wpi.first.wpilibj.SPI;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    
-    public static class DriveTrain {
-        /* PID VALUES FOR ROTATING TO ANGLE */
-        public static final double kP = 0.05;
-        public static final double kI = 0;
-        public static final double kD = 0.0075;
-        /* MOTOR ID NUMBERS */
-        public static final int FRONT_LEFT_ID = 1;
-        public static final int REAR_LEFT_ID = 2;
-        public static final int FRONT_RIGHT_ID = 3;
-        public static final int REAR_RIGHT_ID = 4;
-        public static final int LEFT = 13;
-        public static final int RIGHT = 21;
-        public static final int LEFT_SLIDER = 31;
-        public static final int RIGHT_SLIDER = 32;
-        /* MOTOR TYPES */
-        public static final MotorType FRONT_LEFT_TYPE = MotorType.kBrushless;
-        public static final MotorType REAR_LEFT_TYPE = MotorType.kBrushless;
-        public static final MotorType FRONT_RIGHT_TYPE = MotorType.kBrushless;
-        public static final MotorType REAR_RIGHT_TYPE = MotorType.kBrushless;
-        /* MOTOR INVERSION */
-        public static final boolean LEFT_GROUP_INVERTED = true;
-        public static final boolean RIGHT_GROUP_INVERTED = false;
-        /* ROBORIO PORTS */
-        public static final SPI.Port GYRO_PORT = SPI.Port.kMXP;
-        /* MATH */
-        public static final double FEET_DIVISOR = 0.149;
-    }
 
-    public static class Gripper {
-        public static final int COMPRESSOR_ID = 0;
-    }
+  public static final class OI {
+    public static int JOYSTICK1 = 0;
+    public static int JOYSTICK2 = 1;
+    public static int CONTROLLER = 2;
+  }
 
-    public static class ColorConstants {
-        public static final double RED = -0.31;
-        public static final double BLUE = -0.29;
-      }
-
-      public static class Limelight {
-        public static final float ROTATE_P = -0.1125f;
-        public static final float ROTATE_I = 0f;
-        public static final float ROTATE_D = -0.0075f;
-        
-        public static final float DRIVE_P = -0.15f;
-        public static final float DRIVE_I = -0f;
-        public static final float DRIVE_D = 0.01f;
-      }
+  public static final class CAN {
+    public static int REAR_LEFT_DRIVE = 2;
+    public static int FRONT_LEFT_DRIVE = 5;
+    public static int FRONT_RIGHT_DRIVE = 4;
+    public static int REAR_RIGHT_DRIVE = 3;
+  }
 }
